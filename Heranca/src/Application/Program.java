563f6cef12df90ed3d91cb61e82b05d31adb1eda
Jefferson-Lima-Santos <jeffersonlima2020s@gmail.com>
2023-01-03 18:08:00 -0300

    DownCasting

--- Heranca/src/Application/Program.java
+++ Heranca/src/Application/Program.java
@@ -23,6 +23,21 @@ public class Program {
 		//A ide reclama que não é possível fazer a conversão da super para a sub, mas, se o fizer manualmente
 		//Acionando o (BusinessAccount) logo atras da variavel, a IDE remove o erro, no caso um casting manual
 		BusinessAccount acc4 = (BusinessAccount)acc2;
+		acc4.loan(100.0);
+		// Nesse caso não é possível ja que eu passei um businessAccount sendo que acc3 é um savingsAccount
+		// A ide só retorna o erro na hora de execução
+		//BusinessAccount acc5 = (BusinessAccount)acc3;
 		
+		//Para evitar esse erro, é possível fazer o uso do instanceOf para realizar a verificação do tipo.
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount)acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		if (acc3 instanceof SavingsAccount) {
+			SavingsAccount acc5 = (SavingsAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 	}
 }
